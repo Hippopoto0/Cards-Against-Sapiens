c25faad4ada4e2906b280391ca4a7f5af027837a
Hippopoto0 <67485469+Hippopoto0@users.noreply.github.com>
2022-12-27 16:52:10 +0000

    updates not commited in a while

--- frontend/src/routes/Base.tsx
+++ frontend/src/routes/Base.tsx
@@ -1,28 +1,35 @@
 import { motion } from 'framer-motion'
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { clientID, ws } from '../stores/webSocket'
 
 function App() {
   // const [ws, setWS] = useState<WebSocket>()
   const [prompt, setPrompt] = useState("")
   const [answers, setAnswers] = useState<string[]>([])
-  let client_id = Date.now().toString()
-  let ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`)
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const navigate = useNavigate()
+
+  const roomID = searchParams.get("roomID")
+  // const [client_id, setClient_id] = useState("")
+  // const [ws, setWs] = useState<WebSocket>();
+  // const client_id = Date.now().toString()
 
   useLayoutEffect(() => {
+    console.log(clientID)
     ws.onmessage = (e: MessageEvent<String>) => {
       // console.log(e.data)
       let [header, content] = e.data.split("||")
-      console.log(header)
       if (header === "receive_connection") {
         ws.send(`join_room||`)
+        console.log("clientid" + clientID)
       }
       if (header === "receive_room") {
-        console.log(content)
         ws.send(`get_prompt||`)
         ws.send(`get_answers||`)
       }
       if (header === "receive_prompt") {
-        console.log(content)
         setPrompt(content)
       }
       if (header === "receive_answers") {
@@ -32,10 +39,19 @@ function App() {
         });
 
         setAnswers(result)
-        console.log(JSON.parse(content))
+      }
+      if (header === "receive_goto_selection") {
+        console.log("Gotta go lol")
+        console.log(content)
+        navigate(`/select?roomID=${roomID}`)
       }
     }
   }, [])
+
+  function commitCard(cardText: string) {
+    console.log(clientID)
+    ws.send(`commit_card||${cardText}`)
+  }
 
   return (
     <main className=' w-full bg-secondary center overflow-x-hidden'>
@@ -49,7 +65,8 @@ function App() {
           {answers.map((cardText: string, i: number) => 
             <motion.div 
               key={i} 
-              className='select-none p-2 text-sm font-semibold text-zinc-800 w-32 aspect-[3/4] bg-primary rounded-xl shadow-md hover:cursor-pointer'
+              className='select-none p-2 text-sm font-semibold text-zinc-800 w-32 aspect-[3/4] bg-primary rounded-xl shadow-sm border-2 border-zinc-300 hover:cursor-pointer'
+              onClick={() => commitCard(cardText)}
             >
               {cardText}
             </motion.div>
---
+++ frontend/src/stores/webSocket.ts
@@ -0,0 +1,26 @@
+import create from 'zustand'
+
+export const clientID = Date.now().toString()
+export const ws = new WebSocket(`ws://localhost:8000/ws/${clientID}`)
+
+// interface WebSocketState {
+//     ws: WebSocket,
+//     clientID: string
+// }
+
+// let clientID = Date.now().toString()
+
+// const useWebSocketStore = create<WebSocketState>()((set) => ({
+//     ws: new WebSocket(`ws://localhost:8000/ws/${clientID}`),
+//     clientID: clientID
+// }))
+
+interface BearState {
+  bears: number
+  increase: (by: number) => void
+}
+
+const useBearStore = create<BearState>()((set) => ({
+  bears: 0,
+  increase: (by) => set((state) => ({ bears: state.bears + by })),
+}))
